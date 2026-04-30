dd0acf9188f064950e8737f25f68948cad18e0f6
vinc3calo <vince.calo@ustp.edu.ph>
2026-04-30 12:35:00 +0800

    add middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_ALL
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
