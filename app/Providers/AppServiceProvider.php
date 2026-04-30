28ddc3a530fab4dcdeb9b21aef2f2b8b28c82868
vinc3creed <vince.calo@ustp.edu.ph>
2026-04-30 20:20:49 +0800

    cleanup baseline

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
